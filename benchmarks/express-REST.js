7c615e5337ccdd983436c6c87409a4313d5a0f18
Ben Awad <benawad97@gmail.com>
2020-01-05 15:54:23 -0600

    express post

--- benchmarks/express-REST.js
+++ benchmarks/express-REST.js
@@ -3,7 +3,7 @@ const { graphqlUploadExpress } = require("graphql-upload");
 const { data } = require("../lib/data");
 
 const app = express();
-app.use("/graphql", graphqlUploadExpress(), (_, res) => {
+app.post("/graphql", graphqlUploadExpress(), (_, res) => {
   res.send(data);
 });
 app.listen(4001);
