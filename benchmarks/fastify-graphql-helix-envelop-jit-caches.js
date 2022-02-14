cf85d8bbe3b02fcf289830c9a1ccad0d6af75d13
Mathieu Acthernoene <zoontek@gmail.com>
2022-02-14 20:10:29 +0100

    Add various fastify benchs

---
+++ benchmarks/fastify-graphql-helix-envelop-jit-caches.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const {
+  getGraphQLParameters,
+  processRequest,
+  sendResult,
+} = require("graphql-helix");
+const { envelop, useSchema } = require("@envelop/core");
+const { useGraphQlJit } = require("@envelop/graphql-jit");
+const { useParserCache } = require("@envelop/parser-cache");
+const { useResponseCache } = require("@envelop/response-cache");
+const { useValidationCache } = require("@envelop/validation-cache");
+const fastify = require("fastify");
+const { createSchema } = require("../lib/schema");
+
+const app = fastify();
+const schema = createSchema();
+
+const getEnveloped = envelop({
+  plugins: [
+    useSchema(schema),
+    useGraphQlJit(),
+    useParserCache(),
+    useResponseCache(),
+    useValidationCache(),
+  ],
+});
+
+app.post("/graphql", async (req, res) => {
+  const { parse, validate, contextFactory, execute, schema } = getEnveloped({
+    req,
+  });
+
+  const request = {
+    body: req.body,
+    headers: req.headers,
+    method: req.method,
+    query: req.query,
+  };
+
+  const { operationName, query, variables } = getGraphQLParameters(request);
+
+  const result = await processRequest({
+    operationName,
+    query,
+    variables,
+    request,
+    schema,
+    parse,
+    validate,
+    execute,
+    contextFactory,
+  });
+
+  sendResult(result, res.raw);
+});
+
+app.listen(4001);
